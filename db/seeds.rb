5fbf5f4f52e6447bd369de3f0b38293f26c9f5f9
Lautaro Gartner <158626277+LautaroGartner@users.noreply.github.com>
2026-04-10 16:23:02 -0300

    Fix seed syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.find_or_create_by!(email_address: "lau@mail.com) do |user|
+User.find_or_create_by!(email_address: "lau@mail.com") do |user|
   user.password = "0115"
   user.password_confirmation = "0115"
 end
